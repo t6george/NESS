d62fa7b927f83dfb69f8eab31be1bedf9b0eee40
ThomasGeorge <t6george@edu.uwaterloo.ca>
2019-02-09 23:53:07 -0800

    rectangle drawn with sdl

--- sdltest.c
+++ sdltest.c
@@ -1,29 +1,47 @@
 #include <SDL2/SDL.h>
 
-int main(int argc, char* argv[])
-{
-  SDL_Window *window;                    // Declare a pointer
-  SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
-  // Create an application window with the following settings:
-  window = SDL_CreateWindow(
-      "An SDL2 window",                  // window title
-      SDL_WINDOWPOS_UNDEFINED,           // initial x position
-      SDL_WINDOWPOS_UNDEFINED,           // initial y position
-      640,                               // width, in pixels
-      480,                               // height, in pixels
-      SDL_WINDOW_OPENGL                  // flags - see below
+int main (int argc, char* argv[]) {
+  SDL_Window* window = NULL;
+  window = SDL_CreateWindow
+  (
+      "Jeu de la vie", SDL_WINDOWPOS_UNDEFINED,
+      SDL_WINDOWPOS_UNDEFINED,
+      640,
+      480,
+      SDL_WINDOW_SHOWN
   );
-  // Check that the window was successfully made
-  if (window == NULL) {
-      // In the event that the window could not be made...
-      printf("Could not create window: %s\n", SDL_GetError());
-      return 1;
-  }
-  // The window is open: enter program loop (see SDL_PollEvent)
-  SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
-  // Close and destroy the window
+
+  // Setup renderer
+  SDL_Renderer* renderer = NULL;
+  renderer =  SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
+
+  // Set render color to red ( background will be rendered in this color )
+  SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
+
+  // Clear winow
+  SDL_RenderClear( renderer );
+
+  // Creat a rect at pos ( 50, 50 ) that's 50 pixels wide and 50 pixels high.
+  SDL_Rect r;
+  r.x = 50;
+  r.y = 50;
+  r.w = 50;
+  r.h = 50;
+
+  // Set render color to blue ( rect will be rendered in this color )
+  SDL_SetRenderDrawColor( renderer, 0, 0, 255, 255 );
+
+  // Render rect
+  SDL_RenderFillRect( renderer, &r );
+
+  // Render the rect to the screen
+  SDL_RenderPresent(renderer);
+
+  // Wait for 5 sec
+  SDL_Delay( 5000 );
+
   SDL_DestroyWindow(window);
-  // Clean up
   SDL_Quit();
+
   return 0;
 }
