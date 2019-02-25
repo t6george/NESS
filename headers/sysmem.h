7eb7b7f0923193352779bc6674d35f9d6d951f8d
ThomasGeorge <t6george@edu.uwaterloo.ca>
2019-02-24 18:55:34 -0800

    adding addr modes

--- headers/sysmem.h
+++ headers/sysmem.h
@@ -7,15 +7,31 @@
 #define CART_SPACE_RANGE 0xBFE0
 
 typedef struct {
-	u8 ram [RAM_RANGE];
-	u8 ppuRegs [PPU_REGS_RANGE];
-	u8 apuRegs [APU_REGS_RANGE];
-	u8 apuIORegs [APU_IO_REGS_RANGE];
-	u8 cartridgeMem [CART_SPACE_RANGE];
+	u8 *ram = NULL;
+	u8 *ppuRegs = NULL;
+	u8 *apuRegs = NULL;
+	u8 *apuIORegs = NULL;
+	u8 *cartridgeMem = NULL;
+
+	u8 *cpuRegA = NULL;
+	u8 *cpuRegX = NULL;
+	u8 *cpuRegY = NULL;
+	u8 *cpuRegPC = NULL;
 } mainMemory;
 
+enum addressingMode {
+	ABSOLUTE = 0,
+	ZERO_PAGE,
+	INDIRECT,
+	ABSOLUTE_INDEXED,
+	ZERO_PAGE_INDEXED,
+	INDEXED_INDIRECT,
+	INDIRECT_INDEXED,
+};
 
-mainMemory *initMemory (void);
+
+mainMemory *initMemory (cpu* mos6502);
+void uninitMemory (void);
 u8* decodeAddress (u16 address, mainMemory *memory);
 u8 readByte (u16 address, u8 addrMode, mainMemory *memory);
 void writeByte (u8 data, u16 address, u8 addrMode, mainMemory *memory);
--- src/sysmem.c
+++ src/sysmem.c
@@ -3,10 +3,22 @@
 
 #include "../headers/sysmem.h"
 
-mainMemory *initMemory (void) {
+mainMemory *initMemory (cpu* mos6502) {
 	mainMemory *mem;
-	memset ((void*) mem->ram, 0xFF, RAM_RANGE);
-	memset ((void*) mem->apuRegs, 0x00, APU_REGS_RANGE);
+
+	mem->ram = (u8*) malloc (RAM_RANGE);
+	mem->ppuRegs (u8*) malloc (PPU_REGS_RANGE);
+	mem->apuRegs (u8*) malloc (APU_REGS_RANGE);
+	mem->apuIORegs (u8*) malloc (APU_IO_REGS_RANGE);
+	mem->cartridgeMem (u8*) malloc (CART_SPACE_RANGE);
+
+	memset ((void*) mem, 0x0, sizeof(mem));
+
+	mem->cpuRegA = mos6502->regA;
+	mem->cpuRegX = mos6502->regX;
+	mem->cpuRegY = mos6502->regY;
+	mem->cpuRegPC = mos6502->regPC;
+
 }
 
 u8* decodeAddress (u16 address, mainMemory *memory) {
@@ -26,9 +38,48 @@ u8* decodeAddress (u16 address, mainMemory *memory) {
 }
 
 u8 readByte (u16 address, u8 addrMode, mainMemory *memory) {
+	u8 *memoryContents = NULL;
 
+	switch (addrMode) {
+		case ABSOLUTE: { //account for little endian
+			memoryContents = decodeAddress ((u16) (((address & 0xFF) << 8) | (address >> 8)), memory);
+			break;
+		}
+		case ZERO_PAGE: {
+			memoryContents = decodeAddress (address, memory);
+			break;
+		}
+		case INDIRECT: {
+			memory->cpuRegPC = 
+			break;
+		}
+		case ABSOLUTE_INDEXED: {
+			break;
+		}
+		case ZERO_PAGE_INDEXED: {
+			break;
+		}
+		case INDEXED_INDIRECT: {
+			break;
+		}
+		case INDIRECT_INDEXED: {
+			break;
+		}
+		default: {
+			break;
+		}
+	}
+	return *memoryContents;
 }
 
 void writeByte (u8 data, u16 address, u8 addrMode, mainMemory *memory) {
 
+}
+
+void uninitMemory (void) {
+	free ((void*) mem->ram);
+	free ((void*) mem->ppuRegs);
+	free ((void*) mem->apuRegs);
+	free ((void*) mem->apuIORegs);
+	free ((void*) mem->cartridgeMem);
 }
