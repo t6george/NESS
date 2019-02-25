4463309cd44fe8a3e5d3adfb6e9371f49b0e61ed
ThomasGeorge <t6george@edu.uwaterloo.ca>
2019-02-24 16:29:15 -0800

    mem map implemented

--- headers/sysmem.h
+++ headers/sysmem.h
@@ -2,26 +2,20 @@
 
 #define RAM_RANGE 0x800
 #define PPU_REGS_RANGE 0x8
+#define APU_IO_REGS_RANGE 0x8
 #define APU_REGS_RANGE 0x18
 #define CART_SPACE_RANGE 0xBFE0
-#define MEM_MAP_COUNT 0x4
-
-typedef union {
-	u8 singleByte[1];
-	u8 doubleByte[2];
-} addr;
 
 typedef struct {
-	u8* memory [MEM_MAP_COUNT];
 	u8 ram [RAM_RANGE];
 	u8 ppuRegs [PPU_REGS_RANGE];
 	u8 apuRegs [APU_REGS_RANGE];
+	u8 apuIORegs [APU_IO_REGS_RANGE];
 	u8 cartridgeMem [CART_SPACE_RANGE];
 } mainMemory;
 
 
 mainMemory *initMemory (void);
-u8** decodeAddress (addr address);
-u8 readByte (addr address, u8 addrMode);
-void writeByte (u8 data, addr address, u8 addrMode);
-
+u8* decodeAddress (u16 address, mainMemory *memory);
+u8 readByte (u16 address, u8 addrMode, mainMemory *memory);
+void writeByte (u8 data, u16 address, u8 addrMode, mainMemory *memory);
--- src/sysmem.c
+++ src/sysmem.c
@@ -7,22 +7,28 @@ mainMemory *initMemory (void) {
 	mainMemory *mem;
 	memset ((void*) mem->ram, 0xFF, RAM_RANGE);
 	memset ((void*) mem->apuRegs, 0x00, APU_REGS_RANGE);
-	
-	memory[0] = ram;
-	memory[1] = ppuRegs;
-	memory[2] = apuRegs;
-	memory[3] = cartridgeMem;
 }
 
-u8** decodeAddress (addr address) {
-	
+u8* decodeAddress (u16 address, mainMemory *memory) {
+	u8* memPtr;
+	if (0xE000 & address == 0x0) {
+		memPtr = memory->ram + (0x07FF & address);
+	} else if (0xE000 & address == 0x2000) {
+		memPtr = memory->ppuRegs + (0x0007 & address);
+	} else if (0xFFF0 & address == 0x4000 || 0xFFF8 & address == 0x4010) {
+		memPtr = memory->apuRegs + (0x001F & address);
+	} else if (0xFFF8 & address == 0x4018) {
+		memPtr = memory->apuIORegs + (0x0007 & address);
+	} else {
+		memPtr = memory->cartridgeMem + (address - 0x4020);
+	}
+	return memPtr;
+}
+
+u8 readByte (u16 address, u8 addrMode, mainMemory *memory) {
 
 }
 
-u8 readByte (addr address, u8 addrMode) {
-		
-}
-
-void writeByte (u8 data, addr address, u8 addrMode) {
+void writeByte (u8 data, u16 address, u8 addrMode, mainMemory *memory) {
 
 }
