fd3b7a8408c161b7d5b7f698aac85da54397fee5
ThomasGeorge <t6george@edu.uwaterloo.ca>
2019-12-05 00:04:40 -0500

    implemented mmu class, this time for good XD

---
+++ src/memory/MMU.cpp
@@ -0,0 +1,39 @@
+#include <MMU.hpp>
+#include <AddressableDevice.hpp>
+
+MMU::MMU(const std::vector<AddressingInfo> &table) : virtToPhys{table} {}
+
+void MMU::addEntry(AddressingInfo entry)
+{
+    virtToPhys.emplace_back(entry);
+}
+
+uint8_t MMU::read(uint16_t addr) const
+{
+    uint8_t data = 0x00;
+
+    for (size_t i = 0; i < virtToPhys.size(); ++i)
+    {
+        if (addr <= virtToPhys[i].limit && addr >= virtToPhys[i].base)
+        {
+            addr -= virtToPhys[i].base;
+            data = virtToPhys[i].device->read(addr, virtToPhys[i].mirror);
+            break;
+        }
+    }
+
+    return data;
+}
+
+void MMU::write(uint16_t addr, uint8_t data) const
+{
+    for (size_t i = 0; i < virtToPhys.size(); ++i)
+    {
+        if (addr <= virtToPhys[i].limit && addr >= virtToPhys[i].base)
+        {
+            addr -= virtToPhys[i].base;
+            data = virtToPhys[i].device->write(addr, virtToPhys[i].mirror, data);
+            break;
+        }
+    }
+}
---
+++ src/memory/MMU.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+#include <memory>
+
+class AddressableDevice;
+
+typedef struct
+{
+    uint16_t base;
+    uint16_t limit;
+    uint16_t mirror;
+    std::shared_ptr<AddressableDevice> device;
+} AddressingInfo;
+
+class MMU
+{
+    std::vector<AddressingInfo> virtToPhys;
+
+public:
+    MMU(const std::vector<AddressingInfo> &table);
+    ~MMU() = default;
+
+    void addEntry(AddressingInfo entry);
+    uint8_t read(uint16_t addr) const;
+    void write(uint16_t addr, uint8_t data) const;
+};
